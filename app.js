0de49142f39c945279f7b928959a6288a45f3979
Eric Callari <ericcallari1@gmail.com>
2018-10-04 20:32:40 -0700

    restarted

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+var App = () => (
+  <div>
+    <h3>Grocery List</h3>
+    <Items items={['beer', 'more beer']}/>
+  </div>
+);
+
+var Items = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+
+    
+  </ul>
+);
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
